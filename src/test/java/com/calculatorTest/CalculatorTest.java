704964dea6ce37a525107818b97036cf97649adf
NeoGuRi95 <jea5158@gmail.com>
2022-07-05 11:25:24 +0900

    뺴기 기능 구현

--- src/test/java/com/calculatorTest/CalculatorTest.java
+++ src/test/java/com/calculatorTest/CalculatorTest.java
@@ -7,22 +7,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CalculatorTest {
     @Test
     public void 더하기_테스트() {
-        int rs = Calculator.add("10 + 20");
+        int rs = Calculator.run("10 + 20");
 
         assertEquals(30, rs);
     }
 
     @Test
     public void 더하기_테스트_2() {
-        int rs = Calculator.add("20 + 20");
+        int rs = Calculator.run("20 + 20");
 
         assertEquals(40, rs);
     }
 
     @Test
     public void 더하기_테스트_3() {
-        int rs = Calculator.add("10 + 10");
+        int rs = Calculator.run("10 + 10");
 
         assertEquals(20, rs);
+    }
+
+    @Test
+    public void 뺴기_테스트() {
+        int rs = Calculator.run("10 - 10");
+
+        assertEquals(0, rs);
     }
 }
